e652fea54cb361354b9db7bd49b96edb804f2479
Chantal Neo <Chantal@hotmail.sg>
2020-04-04 22:02:18 +0800

    Attempting Max Chars

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const splitted = str.split('');
+    const counts = {};
+
+    let highestCount = 0;
+    let maxChar = "";
+
+    for (char of splitted) {
+        counts[char] = counts[char] ? counts[char] + 1 : 1;
+        if (counts[char] > highestCount) {
+            highestCount = counts[char];
+            maxChar = char;
+        }
+    }   
+    return maxChar; 
+}
+
+console.log(maxChar('Chantal'))
 
 module.exports = maxChar;
