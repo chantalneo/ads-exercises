f64817e2c18e4ca7b57fdcac107a45c103ee2b86
Chantal Neo <Chantal@hotmail.sg>
2020-04-04 18:22:59 +0800

    Attempting String Reversal

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = "";
+    
+    const splitted = str.split("");
+    for (let i = splitted.length-1; i >= 0; i--) {
+        reversed = reversed + splitted[i];
+    }
+
+    return reversed;
+}
+
+console.log(reverse("Chantal")); // For debugging purpose with node index.js command in the file directory
 
 module.exports = reverse;
