f008f3a35ace866bf80017231bef0160008c8a07
Chantal Neo <Chantal@hotmail.sg>
2020-04-04 18:34:08 +0800

    Debugger Statements

--- reversestring/index.js
+++ reversestring/index.js
@@ -14,7 +14,8 @@ function reverse(str) {
     return str.split('').reduce((rev, char) => char + rev, ''); // ES6
 }
 
-console.log(reverse("Chantal")); // For debugging purpose with node index.js command in the file directory
+// console.log(reverse("Chantal")); // For debugging purpose with node index.js command in the file directory
+// reverse('Chantal playing with Debugger statement');
 
 module.exports = reverse;
 
@@ -25,6 +26,14 @@ module.exports = reverse;
 //    When reduce runs, it's going to take this starting argument and pass it into this arrow function that as the first argument and then whatever gets returned
 //    from that inner function will be then used as the starting argument for every successive run of the function. In total, the function runs one time for every 
 //    element within the array right there.
+//
+// 2. Debugger steps:
+//    1. Add a 'debugger' statement in your function
+//    2. Call the function manually
+//    3. At the terminal, run 'node inspect index.js'
+//    4. To continue execution of the file, press 'c' then 'enter'
+//    5. To launch a 'repl' session, type 'repl' then 'enter'
+//    6. To exit 'repl', press Control + C
 
 // function reverse(str) { // Personal attempt
 //     let reversed = ""; 
