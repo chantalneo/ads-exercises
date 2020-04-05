0863b03a616e7731765726a43f2e51f3d31dbba7
Chantal Neo <Chantal@hotmail.sg>
2020-04-05 23:27:31 +0800

    Chunk, Solution 2

--- chunk/index.js
+++ chunk/index.js
@@ -9,26 +9,26 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    // Create empty array to hold chunks called chunked
+    // Create empty 'chunked' array
     const chunked = [];
-    // For each element in the unchunked array
-    for (let element of array) {
-        // Retrieve the last element in chunked
-        const last = chunked[chunked.length - 1];
-        // if last element doesn't exist, or if its length is equal to chunk size 
-        if (!last || last.length === size) {
-            // Push a new chunk into 'chunked' with the current element
-            chunked.push([element]);
-        } 
-        // Else add the current element into the chunk
-        else {
-            last.push(element);
-        }
+    // Create 'index' start at 0
+    let index = 0;
+    // While index is less than array.length
+    while (index < array.length) {
+        // Push a slice of length 'size' from 'array' into 'chunked'
+        chunked.push(array.slice(index, index + size));
+        // Add 'size' to 'index' 
+        index += size;
     }
     return chunked;
 }
 
 module.exports = chunk;
+
+// Notes:
+// 1. Using slice function would be helpful for this problem. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+//    const letters = ['a', 'b', 'c', 'd', 'e'];
+//    letters.slice(0, 3) would give you ['a', 'b', 'c'], i.e. from index 0 to 3 (excluding index 3).
 
 // function chunk(array, size) { // Personal attempt
 //     let processed = [];
@@ -40,3 +40,22 @@ module.exports = chunk;
 //     return processed;
 // }
 
+// function chunk(array, size) { // Instructor's solution 1
+//     // Create empty array to hold chunks called chunked
+//     const chunked = [];
+//     // For each element in the unchunked array
+//     for (let element of array) {
+//         // Retrieve the last element in chunked
+//         const last = chunked[chunked.length - 1];
+//         // if last element doesn't exist, or if its length is equal to chunk size 
+//         if (!last || last.length === size) {
+//             // Push a new chunk into 'chunked' with the current element
+//             chunked.push([element]);
+//         } 
+//         // Else add the current element into the chunk
+//         else {
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+// }
