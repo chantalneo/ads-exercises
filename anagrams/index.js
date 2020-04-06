69a2db833800ab36887736d428667d83a710ccb1
Chantal Neo <Chantal@hotmail.sg>
2020-04-06 12:38:05 +0800

    Attempting Anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,28 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const objectifiedStringA = objectifyString(stringA);
+    const objectifiedStringB = objectifyString(stringB);
+
+    return JSON.stringify(objectifiedStringA) === JSON.stringify(objectifiedStringB);
+}
+
+function objectifyString(string) {
+    const regex = /[a-z]/g
+    const processedStringToArray = string.toLowerCase().match(regex).sort()
+    const resultingObject = {};
+    for (character of processedStringToArray) {
+        if (resultingObject[character]) {
+            resultingObject[character]++;
+        } else {
+            resultingObject[character] = 1;
+        }
+    }
+    return resultingObject;
+}
+
+console.log(anagrams("Chantal", "Jerry"));
+console.log(anagrams("erryCh", "Cherry"));
 
 module.exports = anagrams;
