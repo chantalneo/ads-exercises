cd9e30a7734939f8c9462bd0339a84659f33740c
Chantal Neo <Chantal@hotmail.sg>
2020-04-08 13:40:22 +0800

    Vowels, Solution 1

--- vowels/index.js
+++ vowels/index.js
@@ -8,8 +8,23 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    const vowels = str.toLowerCase().match(/[aeiou]/g);
-    return vowels === null ? 0 : vowels.length;
+    let count = 0;
+    // let stringVowels = 'aeiou'; // This is kinda misleading because other developers might think the sequence matter, so we should ideally use an array instead
+    let arrayVowels = ['a', 'e', 'i', 'o', 'u'] // Furthermore, using an array can allow inclusion of a block of multiple characters, e.g., 'abc'
+
+    for (let char of str.toLowerCase()) {
+        // if (arrayVowels.indexOf(char) != -1) { // Personal alternative
+        if (arrayVowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 module.exports = vowels;
+
+// function vowels(str) { // Personal attempt
+//     const vowels = str.toLowerCase().match(/[aeiou]/g);
+//     return vowels === null ? 0 : vowels.length;
+// }
