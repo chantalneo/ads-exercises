583dfce623d866f84600e4338d46ff799d876029
Chantal Neo <Chantal@hotmail.sg>
2020-04-08 13:43:11 +0800

    Vowels, Solution 2

--- vowels/index.js
+++ vowels/index.js
@@ -8,18 +8,8 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    let count = 0;
-    // let stringVowels = 'aeiou'; // This is kinda misleading because other developers might think the sequence matter, so we should ideally use an array instead
-    let arrayVowels = ['a', 'e', 'i', 'o', 'u'] // Furthermore, using an array can allow inclusion of a block of multiple characters, e.g., 'abc'
-
-    for (let char of str.toLowerCase()) {
-        // if (arrayVowels.indexOf(char) != -1) { // Personal alternative
-        if (arrayVowels.includes(char)) {
-            count++;
-        }
-    }
-
-    return count;
+    const matches = str.match(/[aeiou]/gi); // g makes sure we don't stop at the first match and i makes it case insensitive
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
@@ -27,4 +17,19 @@ module.exports = vowels;
 // function vowels(str) { // Personal attempt
 //     const vowels = str.toLowerCase().match(/[aeiou]/g);
 //     return vowels === null ? 0 : vowels.length;
+// }
+
+// function vowels(str) { // Instructor's solution 1
+//     let count = 0;
+//     // let stringVowels = 'aeiou'; // This is kinda misleading because other developers might think the sequence matter, so we should ideally use an array instead
+//     let arrayVowels = ['a', 'e', 'i', 'o', 'u'] // Furthermore, using an array can allow inclusion of a block of multiple characters, e.g., 'abc'
+
+//     for (let char of str.toLowerCase()) {
+//         // if (arrayVowels.indexOf(char) != -1) { // Personal alternative
+//         if (arrayVowels.includes(char)) {
+//             count++;
+//         }
+//     }
+
+//     return count;
 // }
