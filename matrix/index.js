89ba34cbb18e06e5673284fae696220c8e62ae7e
Chantal Neo <Chantal@hotmail.sg>
2020-04-08 20:33:59 +0800

    Matrix Spiral, Solution 1

--- matrix/index.js
+++ matrix/index.js
@@ -16,45 +16,98 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-    const mainArray = [];
+    const results = [];
     for (let i = 0; i < n; i++) {
-        let subArray = new Array(n);
-        mainArray.push(subArray);
+        results.push([]);
     }
-    
-    let startRow = 0;
-    let endRow = n-1;
-    let startColumn = 0;
-    let endColumn = n-1;
 
-    let counter = 1;
-    
+    let counter = 1; 
+    let startColumn = 0;
+    let endColumn = n - 1;
+    let startRow = 0;
+    let endRow = n - 1; 
+
     while (startColumn <= endColumn && startRow <= endRow) {
+        // Top row
         for (let i = startColumn; i <= endColumn; i++) {
-            mainArray[startRow][i] = counter;
+            results[startRow][i] = counter;
             counter++;
         }
         startRow++;
+        
+        // Right column
         for (let i = startRow; i <= endRow; i++) {
-            mainArray[i][endColumn] = counter;
+            results[i][endColumn] = counter;
             counter++;
         }
         endColumn--;
+
+        // Bottom row
         for (let i = endColumn; i >= startColumn; i--) {
-            mainArray[endRow][i] = counter;
+            results[endRow][i] = counter;
             counter++;
         }
         endRow--;
+
+        // Start column
         for (let i = endRow; i >= startRow; i--) {
-            mainArray[i][startColumn] = counter;
+            results[i][startColumn] = counter;
             counter++;
         }
         startColumn++;
     }
-    console.log(mainArray);
-    return mainArray;
+
+    return results;
 }
 
 matrix(4);
 
 module.exports = matrix;
+
+// Notes:
+// 1. Key feature or behavior of arrays that is really important here for our solution is the fact that we can assign values to indices inside of an array
+//    that has not been initialized.
+//    E.g.,
+//    const arr = [];
+//    arr[3] = "Hi there!" 
+//    and arr would become [null, null, null, "Hi there!"]
+
+// function matrix(n) { // Personal solution
+//     const mainArray = [];
+//     for (let i = 0; i < n; i++) {
+//         let subArray = new Array(n);
+//         mainArray.push(subArray);
+//     }
+    
+//     let startRow = 0;
+//     let endRow = n-1;
+//     let startColumn = 0;
+//     let endColumn = n-1;
+
+//     let counter = 1;
+    
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             mainArray[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++;
+//         for (let i = startRow; i <= endRow; i++) {
+//             mainArray[i][endColumn] = counter;
+//             counter++;
+//         }
+//         endColumn--;
+//         for (let i = endColumn; i >= startColumn; i--) {
+//             mainArray[endRow][i] = counter;
+//             counter++;
+//         }
+//         endRow--;
+//         for (let i = endRow; i >= startRow; i--) {
+//             mainArray[i][startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++;
+//     }
+//     console.log(mainArray);
+//     return mainArray;
+// }
