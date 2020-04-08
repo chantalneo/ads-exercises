92f4de101db9e1ae3835a68b5ee155d5e617729b
Chantal Neo <Chantal@hotmail.sg>
2020-04-08 13:26:00 +0800

    Attempting Vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = str.toLowerCase().match(/[aeiou]/g);
+    return vowels === null ? 0 : vowels.length;
+}
 
 module.exports = vowels;
