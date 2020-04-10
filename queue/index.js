d0ad3d7d511890dd0c1bce319616480ef4990423
Chantal Neo <Chantal@hotmail.sg>
2020-04-10 15:05:50 +0800

    The Queue Data Structure

--- queue/index.js
+++ queue/index.js
@@ -4,9 +4,9 @@
 // Adding to the queue should store an element until
 // it is removed
 // --- Examples
-//     const q = new Queue();
-//     q.add(1);
-//     q.remove(); // returns 1;
+//     const q = new Queue();       // Create a new empty queue
+//     q.add(1);                    // Add a record to a queue
+//     q.remove(); // returns 1;    // Remove record at the end of a queue
 
 class Queue {}
 
