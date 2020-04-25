c3f80c8e12c5ab2b44ebb44344cafb41afa6ca90
Chantal Neo <Chantal@hotmail.sg>
2020-04-25 14:58:59 +0800

    Attempting Level Width

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -11,6 +11,37 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+const Node = require('./node');
+
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const result = [];
+    let counter = 0;
+
+    while (arr.length) {
+        const node = arr.shift();
+
+        if (node === 's') {
+            result.push(counter);
+            counter = 0;
+            if (arr.length !== 0) { 
+                arr.push(node) 
+            };
+        } else {
+            counter++;
+            arr.push(...node.children);
+        }
+    }
+
+    return result;
+}
+
+const root = new Node(0);
+root.add(1);
+root.add(2);
+root.add(3);
+root.children[0].add(4);
+root.children[2].add(5);
+console.log(levelWidth(root));
 
 module.exports = levelWidth;
