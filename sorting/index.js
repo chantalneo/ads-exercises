b3263588dd03ef5f86bcc81c225e1847fc40e75a
Chantal Neo <Chantal@hotmail.sg>
2020-09-18 12:20:46 +0800

    SelectionSort Solution

--- sorting/index.js
+++ sorting/index.js
@@ -23,7 +23,32 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    // Implement selection sort, a.k.a. the prove me wrong algo
+    // Pseudo code:
+    // From i = 0 to < array length
+        // Assume the element at 'i' is the least in the array, assign i to 'indexOfMin'
+        // For j from i + 1 to end of array
+            // See if there is an element with lower value
+                // If there is, record its index
+        // If the index of the current element and the index of the 'lowest' element is not the same, swap 'em
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
 
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+
+        if (indexOfMin !== i) {
+            let lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser; 
+        }
+    }
+
+    // Return the sorted array
+    return arr;
 }
 
 function mergeSort(arr) {
